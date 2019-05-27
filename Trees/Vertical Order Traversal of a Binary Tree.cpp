5f6989cdf457342357c693febd1d6d42ab1dacb2
mukul96 <mukulkathpalia@gmail.com>
2019-05-28 00:01:38 +0530

    changed to a better code

--- Trees/Vertical Order Traversal of a Binary Tree.cpp
+++ Trees/Vertical Order Traversal of a Binary Tree.cpp
@@ -9,34 +9,41 @@
  */
 class Solution {
 public:
-    unordered_map<int, unordered_map<int, set<int>>> m;
-void traverse(TreeNode* r, int x, int y) {
-  if (r != nullptr) {
-    m[x][y].insert(r->val);
-    traverse(r->left, x - 1, y + 1);
-    traverse(r->right, x + 1, y + 1);
-  }
-}
-vector<vector<int>> verticalTraversal(TreeNode* r) {
-  vector<vector<int>> res;
-  traverse(r, 0, 0);
-  for (int x = -999; x < 1000; ++x) {
-    auto it = m.find(x);
-    if (it != end(m)) {
-      res.push_back(vector<int>());
-      for (int y = 0; y < 1000; ++y) {
-        auto ity = it->second.find(y);
-        if (ity != end(it->second)) {
-          res.back().insert(end(res.back()), begin(ity->second), end(ity->second));
+    map<int,map<int,vector<int> > > m;
+    void traverse(TreeNode *root,int x,int y){
+        if(root==NULL){
+            return;
+        }else{
+            
+            m[x][y].push_back(root->val);
+            traverse(root->left,x-1,y+1);
+            traverse(root->right,x+1,y+1);
+            
+            
         }
-      }
     }
-  }
-  return res;
-}
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        traverse(root,0,0);
+        vector<vector<int>> res;
+        if(root==NULL){
+            return(res);
+        }
+        for(auto it=m.begin();it!=m.end();it++){
+            vector<int> b;
+            for(auto k=it->second.begin();k!=it->second.end();++k ){
+                vector<int> a=k->second;
+                sort(a.begin(),a.end());
+                for(int i=0;i<a.size();i++){
+                    cout<<a[i]<<"  ";
+                    b.push_back(a[i]);
+                }
+            }
+            res.push_back(b);
+        }
+       
+        return(res);
+    }
 };
-
-
 
 
 
