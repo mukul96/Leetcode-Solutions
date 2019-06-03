da29d0664ba24d9daa141e121c3ac5c9b15c0816
mukul96 <mukulkathpalia@gmail.com>
2019-06-03 23:44:03 +0530

    one heap problem solution edited

--- Heap/K Closest Points to Origin.cpp
+++ Heap/K Closest Points to Origin.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    
+    static bool cmp( vector<int>& a, vector<int>& b){
+        return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];;
+    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        });
+        sort(points.begin(), points.end(), cmp);
         return vector<vector<int>>(points.begin(), points.begin() + K);
     }
 };
