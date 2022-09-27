d6ae026dbc6d26d6288fbe71a59e444cd6c268b6
Mukul Kathpalia <mukulkathpalia@gmail.com>
2022-09-27 16:02:32 +0530

    updated Redundant connection problem

--- Graphs/Redundant Connection.cpp
+++ Graphs/Redundant Connection.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
-   int parent(vector<int> &p, int x) {return p[x]==-1 ? x : parent(p, p[x]);}
-    
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
-    {
-        int es = edges.size();
-        vector<int> p(es+1, -1);
-        vector<int> res(2,-1);
-        for(int i=0; i<es; i++)
-        {
-            int u = edges[i][0], v = edges[i][1];
-            int u_par = parent(p,u), v_par = parent(p,v);
-            if(u_par!=v_par) p[v_par] = u_par;
-            else res = edges[i];
+    int find(vector<int> & parent, int i){
+        if(parent[i]==i){
+            return i;
         }
+        else{
+            return find(parent, parent[i]);
+        }
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& nums) {
+        unordered_map<int,int> mp;
+        int count = 0;
+        vector<int> res;
+        int a=0, b;
+        for(int i=0;i<nums.size();i++){
+            if(!mp[nums[i][0]]){
+                count++;
+                mp[nums[i][0]]=1;
+            }
+            if(!mp[nums[i][1]]){
+                count++;
+                mp[nums[i][1]]=1;
+            }
+        }
+        vector<int> parent(count+1,0);
+        for(int i=1;i<=count;i++){
+            parent[i] = i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int x = find(parent, nums[i][0]);
+            int y = find(parent, nums[i][1]);
+            if(x!=y){
+                parent[y] = x;
+            }
+            else{
+                a = nums[i][0];
+                b = nums[i][1];
+            }
+        }
+        if(a==0){
+            return res;
+        }
+        res.push_back(a);
+        res.push_back(b);
         return res;
     }
 };
