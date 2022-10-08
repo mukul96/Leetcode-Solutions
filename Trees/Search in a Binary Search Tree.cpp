a39a064a3a5e20e746bbe9f492006f326964e18f
Mukul Kathpalia <mukulkathpalia@gmail.com>
2022-10-08 23:58:23 +0530

    updated Search in a Binary Search Tree problem

--- Trees/Search in a Binary Search Tree.cpp
+++ Trees/Search in a Binary Search Tree.cpp
@@ -4,7 +4,9 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -13,14 +15,14 @@ public:
         if(root==NULL){
             return NULL;
         }
-        if(root->val==val){
-            return(root);
+        else if(root->val==val){
+            return root;
         }
-        if(root->val<val){
-            return searchBST(root->right,val);
+        else if(root->val < val){
+            return searchBST(root->right, val);
         }
-        
-        return searchBST(root->left,val);
-        
+        else{
+            return searchBST(root->left, val);
+        }
     }
 };
