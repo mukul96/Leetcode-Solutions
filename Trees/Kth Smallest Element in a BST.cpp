d085e43fb3e7a65cbdf063c2746ea3bee0662520
Mukul Kathpalia <mukulkathpalia@gmail.com>
2022-10-22 00:01:07 +0530

    updated Kth Smallest Element in a BST problem

--- Trees/Kth Smallest Element in a BST.cpp
+++ Trees/Kth Smallest Element in a BST.cpp
@@ -1,9 +1,29 @@
-int kthSmallest(TreeNode* root, int k) {
-    return find(root, k);
-}
-int find(TreeNode* root, int& k) {
-    if (root) {
-        int x = find(root->left, k);
-        return !k ? x : !--k ? root->val : find(root->right, k);
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode * root, int &currentIndex,int k, int &value){
+        if(root){  
+            inorder(root->left, currentIndex, k, value);
+            currentIndex = currentIndex+1;
+            if(currentIndex==k){
+                value = root->val;
+            }
+            inorder(root->right, currentIndex, k, value);
+        }
     }
-}
+    int kthSmallest(TreeNode* root, int k) {
+        int currentIndex = 0, value = -1;
+        inorder(root, currentIndex, k, value);
+        return value;
+    }
+};
