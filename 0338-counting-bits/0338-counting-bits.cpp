711a3e79632179ac258d8443d0fd2ea2da3e514a
Mukul Kathpalia <mukulkathpalia@gmail.com>
2022-10-30 13:58:44 +0530

    Time: 11 ms (47.65%), Space: 7.8 MB (88.21%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-    int countB(int n){
-        int res = 0;
-        while(n!=0){
-            if(n&1){
-                res++;
-            }
-            n/=2;
-        }
-        return res;
-    }
     vector<int> countBits(int n) {
-        vector<int> res;
-        res.push_back(0);
+        vector<int> res(n+1,0);
+        res[0] = 0;
         for(int i=1;i<=n;i++){
-            res.push_back(countB(i));
+            res[i] +=res[i&(i-1)] + 1;
         }
-        
-        // reverse(res.begin(),res.end());
         return res;
-        
     }
 };
