a85f2f5336e8e3e60b494869b03d63b92272b6c5
Mukul Kathpalia <mukulkathpalia@gmail.com>
2022-11-14 23:55:22 +0530

    added Running Sum of 1d Array problem

---
+++ Array/Running Sum of 1d Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> res(nums.size(),0);
+        res[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            res[i] = res[i-1] +nums[i];
+        }
+        return res;
+    }
+};
